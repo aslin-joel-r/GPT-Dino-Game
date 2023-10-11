73c1dadfb876c15b577ba7db7915b3c62b844f53
aslin-r <aslinjoelr123@gmail.com>
2023-10-11 20:48:22 +0530

    its moving

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-const dino = document.getElementById("dino");
-const obstacle = document.getElementById("obstacle");
+let dino = document.getElementById("dino");
+let obstacle = document.querySelector(".obstacle");
+let gameContainer = document.getElementById("game-container");
+let isJumping = false;
+
+function startGame() {
+    document.querySelector("button").disabled = true;
+    jump();
+    createObstacle();
+}
 
 function jump() {
-    if (dino.classList != "jump") {
-        dino.classList.add("jump");
-        setTimeout(function () {
-            dino.classList.remove("jump");
-        }, 300);
+    if (!isJumping) {
+        isJumping = true;
+        let jumpHeight = 0;
+        let jumpInterval = setInterval(function() {
+            if (jumpHeight < 100) {
+                dino.style.bottom = jumpHeight + "px";
+                jumpHeight += 10;
+            } else {
+                clearInterval(jumpInterval);
+                let fallDownInterval = setInterval(function() {
+                    if (jumpHeight > 0) {
+                        dino.style.bottom = jumpHeight + "px";
+                        jumpHeight -= 10;
+                    } else {
+                        clearInterval(fallDownInterval);
+                        isJumping = false;
+                    }
+                }, 50);
+            }
+        }, 50);
     }
 }
 
-document.addEventListener("keydown", function (event) {
-    if (event.key === " ") {
-        jump();
-    }
-});
+function createObstacle() {
+    let obstacleLeft = 500;
+    let obstacleInterval = setInterval(function() {
+        if (obstacleLeft > -20) {
+            obstacle.style.left = obstacleLeft + "px";
+            obstacleLeft -= 10;
+        } else {
+            clearInterval(obstacleInterval);
+            obstacleLeft = 500;
+            createObstacle();
+        }
 
-function checkCollision() {
-    const dinoBottom = parseInt(
-        window.getComputedStyle(dino).getPropertyValue("bottom")
-    );
-    const obstacleLeft = parseInt(
-        window.getComputedStyle(obstacle).getPropertyValue("left")
-    );
-
-    if (obstacleLeft < 50 && obstacleLeft > 0 && dinoBottom <= 50) {
-        alert("Game Over!");
-    }
+        if (obstacleLeft < 80 && obstacleLeft > 0 && dino.style.bottom == "0px") {
+            alert("Game Over!");
+            document.location.reload();
+        }
+    }, 50);
 }
-
-setInterval(checkCollision, 10);
